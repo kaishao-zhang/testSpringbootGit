a4d4fc25010ef739e9ebca0d2fa90d3c6e862d85
zyk <1484820861@qq.com>
2019-11-16 17:54:21 +0800

    mavenGit的初始化版本

---
+++ src/main/java/com/atguigu/test/mavengit/MavengitApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.test.mavengit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavengitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavengitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/test/mavengit/MavengitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.test.mavengit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavengitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
